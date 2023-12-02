a7b1ae77e90928d963c92c56eb2258e40bfe9644
jbjzeehad <ilamta.97@gmail.com>
2023-12-02 23:20:41 +0600

    hosting server side in firebase

---
+++ brandshop-client/src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBdBt2xJacr8ROsEy8lrLJvGfJpnWYvsLs",
+    authDomain: "brandshop-client-bed47.firebaseapp.com",
+    projectId: "brandshop-client-bed47",
+    storageBucket: "brandshop-client-bed47.appspot.com",
+    messagingSenderId: "967223018056",
+    appId: "1:967223018056:web:1cb697be444fe96f148e7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
