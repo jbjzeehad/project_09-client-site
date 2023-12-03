2fa1a929ac75d81bc347b0d9d3dfde0d05ca1c5c
jbjzeehad <ilamta.97@gmail.com>
2023-12-03 16:14:43 +0600

    added navbar

---
+++ brandshop-client/src/component/Header/Header.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return (
+        <div className="border grid grid-cols-4 items-center">
+            <div className='ml-20 w-2/5'>
+                <Link to='/'><img className=" w-full" src="logo.png" /></Link>
+            </div>
+            <div className=" col-span-3 flex justify-end gap-8 mr-14">
+                <Link to='/'><button className="border-4 border-transparent focus:border-b-red-700 font-bold text-xl py-1 hover:text-red-700 ">Home</button></Link>
+                <Link to='/features'><button className="border-4 border-transparent focus:border-b-red-700 font-bold text-xl py-1 hover:text-red-700 ">Features</button></Link>
+                <Link to='finddealer'><button className="border-4 border-transparent focus:border-b-red-700 font-bold text-xl py-1 hover:text-red-700 ">Find Dealer</button></Link>
+                <Link to='/signin'><button className="border-4 border-transparent focus:border-b-red-700 font-bold text-xl py-1 hover:text-red-700 ">Sign In</button></Link>
+                <Link to='signup'><button className="border-4 border-transparent focus:border-b-red-700 font-bold text-xl py-1 hover:text-red-700 ">Sign Up</button></Link>
+
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
--- brandshop-client/src/main.jsx
+++ brandshop-client/src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './component/Home/Home.jsx';
+import ErrorPage from './component/ErrorPage/ErrorPage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
