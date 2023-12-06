62a77d8bb94d8e6087d34c682a23e22d62a5c54f
jbjzeehad <ilamta.97@gmail.com>
2023-12-06 15:30:44 +0600

    created home page and extra section

--- brandshop-client/src/component/Accessories/Accessories.jsx
+++ brandshop-client/src/component/Accessories/Accessories.jsx
@@ -8,51 +8,53 @@ const Accessories = () => {
             <h3 className="text-red-700 text-center text-3xl">SHOP NOW</h3>
             <div className="">
                 <div className="p-10 grid grid-cols-4 gap-10">
-                    <div className="border-l-2 border-t-2  rounded-tl-3xl border-black">
-                        <img className="pt-4 px-4" src="logo1.png" />
+                    <div >
+                        <img className="border-l-2 border-t-2  rounded-tl-3xl border-black pt-4 px-4" src="logo1.png" />
                         <div>
                             <button className="w-full py-4 px-5 rounded-br-full bg-black font-bold text-white text-3xl flex items-center gap-3 hover:text-slate-400">NISSAN<LuArrowRightCircle></LuArrowRightCircle></button>
                         </div>
                     </div>
-                    <div className="border-l-2 border-t-2  rounded-tl-3xl border-black">
-                        <img className="pt-4 px-4" src="logo1.png" />
+                    <div>
+                        <img className="border-l-2 border-t-2  rounded-tl-3xl border-black pt-4 px-4" src="logo2.png" />
                         <div>
-                            <button className="w-full py-4 px-5 rounded-br-full bg-black font-bold text-white text-3xl flex items-center gap-3 hover:text-orange-300">NISSAN<LuArrowRightCircle></LuArrowRightCircle></button>
-                        </div>
-                    </div>
-                    <div className="border-l-2 border-t-2  rounded-tl-3xl border-black">
-                        <img className="pt-4 px-4" src="logo1.png" />
-                        <div>
-                            <button className="w-full py-4 px-5 rounded-br-full bg-black font-bold hover:text-blue-300 text-white text-3xl flex items-center gap-3">NISSAN<LuArrowRightCircle></LuArrowRightCircle></button>
+                            <button className="w-full py-4 px-5 rounded-br-full bg-black font-bold text-white text-3xl flex items-center gap-3 hover:text-orange-300">TOYOTA<LuArrowRightCircle></LuArrowRightCircle></button>
                         </div>
                     </div>
                     <div>
-                        <h3>Buy Now</h3>
+                        <img className="border-l-2 border-t-2  rounded-tl-3xl border-black pt-4 px-4" src="logo4.png" />
+                        <div>
+                            <button className="w-full py-4 px-5 rounded-br-full bg-black font-bold hover:text-blue-300 text-white text-3xl flex items-center gap-3">BMW<LuArrowRightCircle></LuArrowRightCircle></button>
+                        </div>
+                    </div>
+                    <div className=" text-center py-10">
+                        <h3 className="text-5xl font-bold">Buy with confidence and <span className="text-red-700">zero</span> pressure.</h3>
                     </div>
                 </div>
 
 
 
                 <div className="p-10 grid grid-cols-4 gap-10">
+                    <div className=" text-center grid justify-items-center py-10">
+                        <h3 className="text-5xl font-bold">Yep. We buy cars, <span className="text-red-700">too</span>.</h3>
+                        <p className="py-3 text-2xl font-semibold">No haggle, no hassle</p>
+                        <button className=" bg-red-600 text-white text-xl font-bold rounded-full py-2 px-4 flex items-center gap-2">TRADE/SELL<LuArrowRightCircle></LuArrowRightCircle></button>
+                    </div>
                     <div>
-                        <h3>Buy Now</h3>
-                    </div>
-                    <div className="border-r-2 border-t-2  rounded-tr-3xl border-black">
-                        <img className="pt-4 px-4" src="logo1.png" />
+                        <img className="border-t-2 border-r-2 rounded-tr-3xl border-black pt-4 px-4" src="logo5.png" />
                         <div>
-                            <button className="w-full py-4 px-5 rounded-bl-full  bg-black font-bold text-white text-3xl flex justify-end items-center gap-3 hover:text-yellow-200"><LuArrowLeftCircle></LuArrowLeftCircle> NISSAN</button>
+                            <button className="w-full py-4 px-5 rounded-bl-full  bg-black font-bold text-white text-3xl flex justify-end items-center gap-3 hover:text-yellow-200"><LuArrowLeftCircle></LuArrowLeftCircle>CHEVROLET</button>
                         </div>
                     </div>
-                    <div className="border-r-2 border-t-2  rounded-tr-3xl border-black">
-                        <img className="pt-4 px-4" src="logo1.png" />
+                    <div>
+                        <img className="border-r-2 border-t-2  rounded-tr-3xl border-black pt-4 px-4" src="logo3.png" />
                         <div>
-                            <button className="w-full py-4 px-5 rounded-bl-full  bg-black font-bold text-white text-3xl flex justify-end items-center gap-3 hover:text-red-400"><LuArrowLeftCircle></LuArrowLeftCircle> NISSAN</button>
+                            <button className="w-full py-4 px-5 rounded-bl-full  bg-black font-bold text-white text-3xl flex justify-end items-center gap-3 hover:text-red-400"><LuArrowLeftCircle></LuArrowLeftCircle>FORD</button>
                         </div>
                     </div>
-                    <div className="border-r-2 border-t-2  rounded-tr-3xl border-black">
-                        <img className="pt-4 px-4" src="logo1.png" />
+                    <div>
+                        <img className="border-r-2 border-t-2  rounded-tr-3xl border-black pt-4 px-4" src="logo6.png" />
                         <div>
-                            <button className="w-full py-4 px-5 rounded-bl-full  bg-black font-bold text-white text-3xl flex justify-end items-center gap-3 hover:text-lime-300"><LuArrowLeftCircle></LuArrowLeftCircle> NISSAN</button>
+                            <button className="w-full py-4 px-5 rounded-bl-full  bg-black font-bold text-white text-3xl flex justify-end items-center gap-3 hover:text-lime-300"><LuArrowLeftCircle></LuArrowLeftCircle>AUDI</button>
                         </div>
                     </div>
 
